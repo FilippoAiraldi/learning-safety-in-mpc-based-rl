2400e63d93057870503177513903ec2db3b9f26b
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-07-24 20:54:06 +0200

    updated train.py

--- train.py
+++ train.py
@@ -1,21 +1,21 @@
 import envs
 import numpy as np
-import matplotlib.pyplot as plt
+import plot
 
-# 
-env = envs.QuadRotorEnv.get_wrapped()
-env.reset()
-done = False
-while not done:
-    obs, r, done, _ = env.step(np.array([0, 0, 18]))
-    
-# 
-fig = plt.figure()
-ax = plt.axes(projection='3d')
-ax.plot3D(
-    env.observations_history[0][0],
-    env.observations_history[0][1], 
-    env.observations_history[0][2])
-plt.show()
 
-print('DONE')
+if __name__ == '__main__':
+    # TODO: parse arguments
+
+    #
+    env = envs.QuadRotorEnv.get_wrapped()
+    env.reset(seed=42)
+    done = False
+    while not done:
+        a = np.array([0, 0, 5])
+        obs, r, done, _ = env.step(a)
+
+    #
+    plot.plot_trajectory(env, 0)
+    plot.show()
+
+    print('DONE')
