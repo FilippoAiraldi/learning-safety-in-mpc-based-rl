0cd0b4b6dadb13857df811f8387cf3637eb89d02
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-07-25 17:50:02 +0200

    improvements to util and plotting constraints

--- util/plot.py
+++ util/plot.py
@@ -1,8 +1,9 @@
 import numpy as np
 from wrappers import RecordData
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d.art3d import Line3DCollection
+from mpl_toolkits.mplot3d.art3d import Line3DCollection, Poly3DCollection
 from matplotlib.ticker import PercentFormatter
+from itertools import product
 
 
 def plot_trajectory(env: RecordData, i: int):
@@ -11,31 +12,50 @@ def plot_trajectory(env: RecordData, i: int):
     y = env.observations_history[i][1]
     z = env.observations_history[i][2]
     x0, xf = env.x0, env.xf
-    fig = plt.figure()
+    fig = plt.figure(figsize=(12, 7))
+    patch_kwargs = {
+        'facecolor': 'k', 'alpha': 0.1, 'edgecolor': 'k', 'linewidth': 2}
 
     # 3d trajectory
     ax = fig.add_subplot(2, 2, 1, projection='3d')
-    # ax.plot3D(x, y, z)
     points = np.array([x, y, z]).T.reshape(-1, 1, 3)
     segments = np.concatenate([points[:-1], points[1:]], axis=1)
-    lc = Line3DCollection(segments, cmap='RdBu_r', norm=plt.Normalize(0, 100))
+    lc = Line3DCollection(
+        segments, cmap='coolwarm', norm=plt.Normalize(0, 100))
     lc.set_array(np.linspace(0, 100, x.size - 1))
     line = ax.add_collection3d(lc)
     ax.scatter(x0[0], x0[1], x0[2], marker='o', color='k')
     ax.scatter(xf[0], xf[1], xf[2], marker='x', color='k')
     fig.colorbar(line, ax=ax, location='left', format=PercentFormatter())
-    ax.set_xlim(min(x.min(), ax.get_xlim()[0]), max(x.max(), ax.get_xlim()[1]))
-    ax.set_ylim(min(y.min(), ax.get_ylim()[0]), max(y.max(), ax.get_ylim()[1]))
-    ax.set_zlim(min(z.min(), ax.get_zlim()[0]), max(z.max(), ax.get_zlim()[1]))
+    #
+    Z = np.array(list(product(*env.x_bounds[:3, :])))
+    verts = [[Z[0], Z[4], Z[6], Z[2]],
+             [Z[1], Z[5], Z[7], Z[3]],
+             [Z[0], Z[4], Z[5], Z[1]],
+             [Z[6], Z[2], Z[3], Z[7]],
+             [Z[4], Z[6], Z[7], Z[5]],
+             [Z[1], Z[3], Z[2], Z[0]]]
+    # many thanks to: https://www.pythonpool.com/matplotlib-draw-rectangle/
+    ax.add_collection3d(Poly3DCollection(verts, **patch_kwargs))
+    #
     ax.set_xlabel('Pos: x [m]')
     ax.set_ylabel('Pos: y [m]')
     ax.set_zlabel('Altitude [m]')
+    ax.set_xlim(min(x.min(), env.x_bounds[0, 0], ax.get_xlim()[0]),
+                max(x.max(), env.x_bounds[0, 1], ax.get_xlim()[1]))
+    ax.set_ylim(min(y.min(), env.x_bounds[1, 0], ax.get_ylim()[0]),
+                max(y.max(), env.x_bounds[1, 1], ax.get_ylim()[1]))
+    ax.set_zlim(min(z.min(), env.x_bounds[2, 0], ax.get_zlim()[0]),
+                max(z.max(), env.x_bounds[2, 1], ax.get_zlim()[1]))
 
     # y-z trajectory
     ax = fig.add_subplot(2, 2, 2)
     ax.plot(y, z)
     ax.plot(x0[1], x0[2], marker='o', color='k')
     ax.plot(xf[1], xf[2], marker='x', color='k')
+    ax.add_patch(plt.Rectangle(env.x_bounds[(1, 2), 0],
+                               *np.diff(env.x_bounds[(1, 2), :]), 
+                               **patch_kwargs))
     ax.set_xlabel('Pos: y [m]')
     ax.set_ylabel('Altitude [m]')
 
@@ -44,6 +64,9 @@ def plot_trajectory(env: RecordData, i: int):
     ax.plot(x, y)
     ax.plot(x0[0], x0[1], marker='o', color='k')
     ax.plot(xf[0], xf[1], marker='x', color='k')
+    ax.add_patch(plt.Rectangle(env.x_bounds[(0, 1), 0],
+                               *np.diff(env.x_bounds[(0, 1), :]), 
+                               **patch_kwargs))
     ax.set_xlabel('Pos: x [m]')
     ax.set_ylabel('Pos: y [m]')
 
@@ -52,83 +75,11 @@ def plot_trajectory(env: RecordData, i: int):
     ax.plot(x, z)
     ax.plot(x0[0], x0[2], marker='o', color='k')
     ax.plot(xf[0], xf[2], marker='x', color='k')
+    ax.add_patch(plt.Rectangle(env.x_bounds[(0, 2), 0],
+                               *np.diff(env.x_bounds[(0, 2), :]), 
+                               **patch_kwargs))
     ax.set_xlabel('Pos: x [m]')
     ax.set_ylabel('Altitude [m]')
 
     fig.subplots_adjust(wspace=0.3, hspace=0.3)
-
-
-def show():
-    return plt.show()
-
-###############################################################################
-# import numpy as np
-# import casadi as cs
-
-
-# def expK(
-#     x: np.ndarray | cs.SX | cs.MX | cs.DM,
-#     c: np.ndarray,
-#     l: float
-# ) -> np.ndarray | cs.SX | cs.MX | cs.DM:
-#     '''
-#     Exponential Radial Basis Function.
-
-#     Parameters
-#     ----------
-#     x : array_like
-#         Values at which to evaluate the function.
-#     c : array_like
-#         Center of the function. Size must be compatible with x.
-#     l : float
-#         Length factor of the function.
-
-#     Returns
-#     -------
-#     y : scalar
-#         The value of the function, computed as y=exp(-|x-c|^2 / 2l).
-#     '''
-#     x_ = x - c
-#     return np.exp(-(x_.T @ x_ / (2 * l)))
-
-
-# def phi_pos(
-#     x: np.ndarray | cs.SX | cs.MX | cs.DM,
-#     C: tuple[np.ndarray],
-#     l: float = 15
-# ) -> np.ndarray:
-#     '''TODO'''
-#     phi = tuple(expK(x, c, l) for c in C)
-#     return cs.vertcat(*phi) if is_casadi_type(x) else np.vstack(phi)
-
-
-# def is_casadi_type(array_like, recursive: bool = False) -> bool:
-#     '''
-#     Returns a boolean of whether an object is a CasADi data type or not. If the
-#     recursive flag is True, iterates recursively.
-
-#     Parameters
-#     ----------
-#     array_like : array_like
-#         The object to evaluate. Either from numpy or casadi.
-
-#     recursive : bool
-#         If the object is a list or tuple, recursively iterate through every
-#         subelement. If any of the subelements are a CasADi type, return True.
-#         Otherwise, return False.
-
-#     Returns
-#     ----------
-#     is_casadi : bool
-#         A boolean if the object is a CasADi data type.
-#     '''
-#     if recursive and isinstance(array_like, (list, tuple)):
-#         for element in array_like:
-#             if is_casadi_type(element, recursive=True):
-#                 return True
-
-#     return (
-#         isinstance(array_like, cs.MX) or
-#         isinstance(array_like, cs.DM) or
-#         isinstance(array_like, cs.SX)
-#     )
+    plt.show(block=False)
--- util/util.py
+++ util/util.py
@@ -10,7 +10,8 @@ def quad_form(A: cs.SX | cs.DM, x: cs.SX | cs.DM) -> cs.SX | cs.DM:
     return cs.bilin(A, x, x)
 
 
-def spy(H: cs.SX | cs.DM | np.ndarray, **kwargs):
+def spy(H: cs.SX | cs.DM | np.ndarray, **original_kwargs):
+    '''See Matplotlib.pyplot.spy.'''
     # try convert to numerical; if it fails, then use symbolic method from cs
     try:
         H = np.array(H)
@@ -24,7 +25,81 @@ def spy(H: cs.SX | cs.DM | np.ndarray, **kwargs):
         H = np.array([list(line) for line in 
                       out.replace('.', '0').replace('*', '1').splitlines()], 
                       dtype=int)
-    plt.spy(H, **kwargs)
+    plt.spy(H, **original_kwargs)
     nz = np.count_nonzero(H)
     plt.xlabel(f'nz = {nz} ({nz / H.size * 100:.2f}%)')
     plt.show(block=False)
+
+
+
+###############################################################################
+# import numpy as np
+# import casadi as cs
+
+
+# def expK(
+#     x: np.ndarray | cs.SX | cs.MX | cs.DM,
+#     c: np.ndarray,
+#     l: float
+# ) -> np.ndarray | cs.SX | cs.MX | cs.DM:
+#     '''
+#     Exponential Radial Basis Function.
+
+#     Parameters
+#     ----------
+#     x : array_like
+#         Values at which to evaluate the function.
+#     c : array_like
+#         Center of the function. Size must be compatible with x.
+#     l : float
+#         Length factor of the function.
+
+#     Returns
+#     -------
+#     y : scalar
+#         The value of the function, computed as y=exp(-|x-c|^2 / 2l).
+#     '''
+#     x_ = x - c
+#     return np.exp(-(x_.T @ x_ / (2 * l)))
+
+
+# def phi_pos(
+#     x: np.ndarray | cs.SX | cs.MX | cs.DM,
+#     C: tuple[np.ndarray],
+#     l: float = 15
+# ) -> np.ndarray:
+#     '''TODO'''
+#     phi = tuple(expK(x, c, l) for c in C)
+#     return cs.vertcat(*phi) if is_casadi_type(x) else np.vstack(phi)
+
+
+# def is_casadi_type(array_like, recursive: bool = False) -> bool:
+#     '''
+#     Returns a boolean of whether an object is a CasADi data type or not. If the
+#     recursive flag is True, iterates recursively.
+
+#     Parameters
+#     ----------
+#     array_like : array_like
+#         The object to evaluate. Either from numpy or casadi.
+
+#     recursive : bool
+#         If the object is a list or tuple, recursively iterate through every
+#         subelement. If any of the subelements are a CasADi type, return True.
+#         Otherwise, return False.
+
+#     Returns
+#     ----------
+#     is_casadi : bool
+#         A boolean if the object is a CasADi data type.
+#     '''
+#     if recursive and isinstance(array_like, (list, tuple)):
+#         for element in array_like:
+#             if is_casadi_type(element, recursive=True):
+#                 return True
+
+#     return (
+#         isinstance(array_like, cs.MX) or
+#         isinstance(array_like, cs.DM) or
+#         isinstance(array_like, cs.SX)
+#     )
