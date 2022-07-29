6b4698c03ea3e515d952810d2a709e1b08331acb
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-07-29 19:06:35 +0200

    implementing DPG agent (WIP)

--- agents/quad_rotor_DPG_agent.py
+++ agents/quad_rotor_DPG_agent.py
@@ -1,10 +1,11 @@
 import numpy as np
 import casadi as cs
-from envs.quad_rotor_env import QuadRotorEnvConfig, QuadRotorEnv
-from mpc.quad_rotor_mpc import QuadRotorMPCConfig
+from envs import QuadRotorEnvConfig, QuadRotorEnv
+from mpc import Solution, QuadRotorMPCConfig
 from agents.quad_rotor_base_learning_agent import QuadRotorBaseLearningAgent
 from agents.replay_memory import ReplayMemory
 from dataclasses import dataclass
+from scipy.linalg import lstsq
 
 
 @dataclass(frozen=True)
@@ -30,9 +31,9 @@ class QuadRotorDPGAgentConfig:
     init_backoff: float = 0.05
 
     # experience replay parameters
-    replay_maxlen: float = 30 * 10  # more or less 10 episodes
-    replay_sample_size: float = 0.5  # sample from 5 out of 10 episodes
-    replay_include_last: float = 0.2  # include in the sample the last 2 episodes
+    replay_maxlen: float = 20  # 20 episodes
+    replay_sample_size: float = 10  # sample from 10 out of 20 episodes
+    replay_include_last: float = 5  # include in the sample the last 5 episodes
 
     @property
     def init_pars(self) -> dict[str, float | np.ndarray]:
@@ -41,13 +42,6 @@ class QuadRotorDPGAgentConfig:
             name.removeprefix('init_'): val
             for name, val in self.__dict__.items() if name.startswith('init_')
         }
-
-
-@dataclass(frozen=True)
-class Derivatives:
-    dRdtheta: cs.SX
-    dRdy: cs.SX
-    dydu0: cs.SX
 
 
 class QuadRotorDPGAgent(QuadRotorBaseLearningAgent):
@@ -95,20 +89,81 @@ class QuadRotorDPGAgent(QuadRotorBaseLearningAgent):
                          mpc_config=mpc_config)
 
         # initialize the replay memory
-        self.replay_memory = ReplayMemory(maxlen=agent_config.replay_maxlen)
+        self.replay_memory = ReplayMemory[np.ndarray](
+            maxlen=agent_config.replay_maxlen)
 
         # compute the symbolical derivatives needed to perform the DPG updates
         # gather some variables
-        theta = self.weights.symV()  # vector of MPC parameters
-        R, y = self.V.kkt_matrix    # KKT matrix and primal-dual variables
+        theta = self.weights.symV()
+        R, y, _, g_ineq_all = self.V.kkt_conditions
 
         # compute the derivative of the policy (pi) w.r.t. the mpc pars (theta)
         dRdtheta = cs.simplify(cs.jacobian(R, theta)).T
         dRdy = cs.simplify(cs.jacobian(R, y)).T
         dydu0 = cs.simplify(cs.jacobian(y, self.V.vars['u'][:, 0]))
-        self.derivatives = Derivatives(dRdtheta, dRdy, dydu0)
-        # dpidtheta = -dRdtheta @ cs.inv_minor(dRdy) @ dydu0 CANNOT BE DONE SYMBOLICALLY
+        self._sym: dict[str, cs.SX] = {
+            'g_ineq_all': g_ineq_all,
+            'dRdy': dRdy,
+            'dydu0': dydu0,
+            'dRdtheta': dRdtheta
+        }
+        # NOTE: cannot go further with symbolic computations as they get too
+        # heavy due to matrix inversion of dRdy, which tends to be quite big.
 
-        # import time
-        # t0 = time.perf_counter()
-        # tf = time.perf_counter() - t0
+        # initialize small internal buffer of solutions, one per each timestep
+        self._buffer: list[Solution] = []
+
+    def save_transition(self, solV: Solution) -> None:
+        '''Saves the solution at the current time-step.'''
+        self._buffer.append(solV)
+
+    def consolidate_episode_experience(self) -> None:
+        # extract the symbols
+        g_ineq_all = self._sym['g_ineq_all']
+        dRdy = self._sym['dRdy']
+        dydu0 = self._sym['dydu0']
+        dRdtheta = self._sym['dRdtheta']
+
+        # pre-compute constants
+        offset = self.V.nx + self.V.ng_eq
+        dLdw_and_g_eq_idx = np.arange(offset)
+
+        # for each solution, make the computations
+        # TODO: check if parallelizing this loop helps
+        dpidthetas = []
+        for sol in self._buffer:
+            # get active constraints only
+            idx2 = np.where(np.concatenate((
+                [True] * offset,
+                np.isclose(sol.value(g_ineq_all), 0)
+            )))[0]
+            idx = np.concatenate((
+                dLdw_and_g_eq_idx,
+                offset + np.where(np.isclose(sol.value(g_ineq_all), 0))[0]
+            ))
+            # TODO: check these are equal
+            assert (idx == idx2).all()
+
+            # compute the derivative of the policy w.r.t. the mpc weights theta
+            dRdy = sol.value(dRdy[idx, idx])
+            dydu0 = sol.value(dydu0[idx, :])
+            dRdtheta = sol.value(dRdtheta[:, idx])
+            dpidthetas.append(
+                -dRdtheta @ lstsq(dRdy, dydu0, lapack_driver='gelsy')[0])
+
+        # save to replay memory
+        # TODO: compact dpidthetas to an array
+        # TODO: save also the state in which the solution was computed
+
+        # finally, clear the temp buffer
+        self._buffer.clear()
+
+# #
+# # q0 = np.linalg.solve(dRdys[-1], dydu0s[-1])
+# #
+# q1 = lstsq(dRdys[-1], dydu0s[-1], lapack_driver='gelsy')[0]
+# #
+# U2, S2, V2T = np.linalg.svd(dRdys[-1])
+# y2 = np.linalg.solve(np.diag(S2), U2.T @ dydu0s[-1])
+# q2 = np.linalg.solve(V2T, y2)
+# #
--- train.py
+++ train.py
@@ -4,6 +4,14 @@ import util
 from tqdm import tqdm
 
 
+# TODO:
+1. record at each time step the quantities above, or record the solution object, in an internal temporary buffer of the agent
+2. at the end of an episode, consolidate these quantities inside the replaymem (in order to maintain the separation between episodes!)
+- perform solution.value and lstsq to find dpidtheta
+- save everthing to replay memory
+3. then call update
+
+
 if __name__ == '__main__':
     # TODO: argparse
     # - fix seed
@@ -14,17 +22,15 @@ if __name__ == '__main__':
     max_episode_steps = 50
     env = envs.QuadRotorEnv.get_wrapped(
         max_episode_steps=max_episode_steps)
-    #
+
     # agent = agents.QuadRotorPIAgent(env=env, agentname='PI')
-    #
     agent = agents.QuadRotorDPGAgent(env=env, agentname='DPG')
-    #
 
     # simulate
     env.reset(seed=4)
     for _ in tqdm(range(max_episode_steps), total=max_episode_steps):
         # compute V(s)
-        u_opt, _, solution = agent.predict(deterministic=True)
+        u_opt, _, solution = agent.predict(deterministic=False)
         assert solution.success
 
         # step environment
