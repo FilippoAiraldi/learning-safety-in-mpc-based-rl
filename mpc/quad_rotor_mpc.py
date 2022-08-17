c7664e43db7ed2d069288b12d98f4befff0de8ae
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-08-17 17:52:27 +0200

    fine-tuned scaling (also inverted)
    (cherry picked from commit 71bfab760ee8aba34be6cb4b413530f85e4d2cf8)

--- mpc/quad_rotor_mpc.py
+++ mpc/quad_rotor_mpc.py
@@ -2,7 +2,6 @@ import casadi as cs
 import numpy as np
 from dataclasses import dataclass, field
 from envs.quad_rotor_env import QuadRotorEnv
-from functools import cached_property
 from mpc.generic_mpc import GenericMPC, Solution
 from typing import Union
 from util import quad_form
@@ -32,23 +31,24 @@ class QuadRotorMPCConfig:
     # NLP scaling
     # The scaling operation is x_scaled = Tx * x, and yields a scaled state
     # whose elements lay in comparable ranges
-    scaling_x: list[float] = field(default_factory=lambda: 
-        [1e-1, 1e-1, 1e-1, 1e-1, 1e-1, 1e-1, 1, 1, 1e-1, 1e-1])
-    scaling_u: list[float] = field(default_factory=lambda: [1e-1, 1e-1, 1e-2])
+    scaling_x: np.ndarray = field(default_factory=lambda: np.array(
+        [1e0, 1e0, 1e0, 1e1, 1e1, 1e1, 1e-1, 1e-1, 1e0, 1e0]))
+    scaling_u: np.ndarray = field(default_factory=lambda: np.array(
+        [1e0, 1e0, 1e1]))
 
-    @cached_property
+    @property
     def Tx(self) -> np.ndarray:
+        return np.diag(1 / self.scaling_x)
+
+    @property
+    def Tu(self) -> np.ndarray:
+        return np.diag(1 / self.scaling_u)
+
+    @property
+    def Tx_inv(self) -> np.ndarray:
         return np.diag(self.scaling_x)
 
-    @cached_property
-    def Tu(self) -> np.ndarray:
-        return np.diag(self.scaling_u)
-
-    @cached_property
-    def Tx_inv(self) -> np.ndarray:
-        return np.linalg.inv(self.Tx)
-
-    @cached_property
+    @property
     def Tu_inv(self) -> np.ndarray:
         return np.linalg.inv(self.Tu)
 
