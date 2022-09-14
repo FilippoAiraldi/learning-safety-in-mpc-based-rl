0f8dc6da5da8a6db72f96d0b8785f0bd5971872c
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-09-14 22:26:17 +0200

    forgot to remove one failure

--- agents/__init__.py
+++ agents/__init__.py
@@ -4,7 +4,5 @@ from agents.rl_parameters import RLParameter, RLParameterCollection
 from agents.quad_rotor_PI_agent import QuadRotorPIAgent
 from agents.quad_rotor_LSTD_Q_agent import (
     QuadRotorLSTDQAgent, QuadRotorLSTDQAgentConfig)
-from agents.linear_LSTD_DPG_agent import (
-    LinearLSTDDPGAgent, LinearLSTDDPGAgentConfig)
 
 from agents import wrappers
