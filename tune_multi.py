ae5adbbe4d17e9cc3b73d5ae276e7a327f77aa4b
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-09-15 10:51:16 +0200

    increased parallel computations in tune_multi.py

--- tune_multi.py
+++ tune_multi.py
@@ -1,9 +1,118 @@
+import contextlib
+import joblib as jl
 import numpy as np
 import optuna
+import os
+import shutil
 import util
 from agents import QuadRotorLSTDQAgent
 from envs import QuadRotorEnv
 from mpc import MPCSolverError
+from typing import Any, Callable
+
+
+# def objective(
+#     trial: optuna.Trial,
+#     n_epochs: int,
+#     n_agents: int,
+#     max_ep_steps: int,
+#     seed: int,
+# ) -> float:
+#     # suggest parameters for this trial
+#     gamma = trial.suggest_float(
+#         'gamma', 0.98, 1.0, log=True)
+#     lr = trial.suggest_float(
+#         'lr', 1e-3, 5e-1, log=True)
+#     train_eps = trial.suggest_categorical(
+#         'train_eps', [1, 5, 10, 20])
+#     max_perc_update = trial.suggest_categorical(
+#         'max_perc_update', [0.2, 0.5, 1.0, np.inf])
+#     replay_maxlen_factor = trial.suggest_int(
+#         'replay_maxlen_factor', 1, n_epochs // 2)
+#     replay_mem_sample_size = trial.suggest_float(
+#         'mem_sample_size', 0.2, 0.8, step=0.1)
+#     replay_include_last_factor = trial.suggest_int(
+#         'replay_include_last_factor', 0, 1)
+#     perturbation_decay = trial.suggest_float(
+#         'perturbation_decay', 0.5, 0.99)
+
+#     # create envs and agents
+#     agent_config = {
+#         'gamma': gamma,
+#         'lr': lr,
+#         'max_perc_update': max_perc_update,
+#         'replay_maxlen': train_eps * replay_maxlen_factor,
+#         'replay_sample_size': replay_mem_sample_size,
+#         'replay_include_last': train_eps * replay_include_last_factor
+#     }
+#     envs: list[QuadRotorEnv] = []
+#     agents: list[QuadRotorLSTDQAgent] = []
+#     for n_agent in range(n_agents):
+#         env = QuadRotorEnv.get_wrapped(
+#             max_episode_steps=max_ep_steps,
+#             record_data=False,
+#             normalize_observation=False,
+#             normalize_reward=False)
+
+#         agent = QuadRotorLSTDQAgent(
+#             env=env,
+#             agentname=f'LSTDQ_{n_agent}',
+#             agent_config=agent_config,
+#             seed=(seed + n_agent) * (n_agent + 1) * 1000)
+#         envs.append(env)
+#         agents.append(agent)
+
+#     # train along each session, and report average performance after each one
+#     for n_epoch in range(n_epochs):
+#         # learn one epoch per agent
+#         returns = [
+#             agent.learn_one_epoch(
+#                 n_episodes=train_eps,
+#                 perturbation_decay=perturbation_decay,
+#                 seed=seed + train_eps * n_epoch,
+#                 raises=True)
+#             for agent in agents
+#         ]
+
+#         # average peformance over last n episodes and over all agents
+#         mean_return = np.mean(returns)
+
+#         # report current average performance
+#         trial.report(mean_return, n_epoch)
+#         if trial.should_prune():
+#             raise optuna.TrialPruned()
+
+#     # return last session's averaged performance as result of the trial
+#     return mean_return
+
+
+def train_one_agent(
+    n_agent: int,
+    n_epochs: int,
+    n_episodes: int,
+    max_ep_steps: int,
+    perturbation_decay: float,
+    agent_config: dict[str, Any],
+    seed: int,
+    after_epoch_callback: Callable[[int, int, float], None]
+) -> None:
+    env = QuadRotorEnv.get_wrapped(
+        max_episode_steps=max_ep_steps,
+        record_data=False,
+        normalize_observation=False,
+        normalize_reward=False)
+    agent = QuadRotorLSTDQAgent(
+        env=env,
+        agentname=f'LSTDQ_{n_agent}',
+        agent_config=agent_config,
+        seed=seed * (n_agent + 1) * 1000)
+    for n_epoch in range(n_epochs):
+        current_returns = agent.learn_one_epoch(
+            n_episodes=n_episodes,
+            perturbation_decay=perturbation_decay,
+            seed=seed + n_episodes * n_epoch,
+            raises=True)
+        after_epoch_callback(n_agent, n_epoch, current_returns.mean())
 
 
 def objective(
@@ -40,55 +149,48 @@ def objective(
         'replay_sample_size': replay_mem_sample_size,
         'replay_include_last': train_eps * replay_include_last_factor
     }
-    envs: list[QuadRotorEnv] = []
-    agents: list[QuadRotorLSTDQAgent] = []
-    for n_agent in range(n_agents):
-        env = QuadRotorEnv.get_wrapped(
-            max_episode_steps=max_ep_steps,
-            record_data=False,
-            normalize_observation=False,
-            normalize_reward=False)
 
-        agent = QuadRotorLSTDQAgent(
-            env=env,
-            agentname=f'LSTDQ_{n_agent}',
+    # setup tracking of intermediate results memmap
+    folder = f'.\\tuning\\joblib_memmap\\trial{trial._trial_id}'
+    with contextlib.suppress(FileNotFoundError):
+        shutil.rmtree(folder)
+    with contextlib.suppress(FileExistsError):
+        os.mkdir(folder)
+    returns = np.memmap(
+        os.path.join(folder, 'returns_memmap'),
+        dtype=float,
+        shape=(n_agents, n_epochs),
+        mode='w+')
+    returns.fill(np.nan)
+
+    def after_epoch_callback(n_agent: int, n_epoch: int, mean_return: float):
+        # store this agent's returns in this epoch
+        returns[n_agent, n_epoch] = mean_return
+
+        # if all agents have gone through this epoch, report its performance
+        if not np.isnan(returns[:, n_epoch]).any():
+            trial.report(returns[:, n_epoch].mean(), n_epoch)
+            if trial.should_prune():
+                raise optuna.TrialPruned()
+
+    # launch parallel training of each agent
+    jl.Parallel(n_jobs=-1)(
+        jl.delayed(train_one_agent)(
+            n_agent=n_agent,
+            n_epochs=n_epochs,
+            n_episodes=train_eps,
+            max_ep_steps=max_ep_steps,
+            perturbation_decay=perturbation_decay,
             agent_config=agent_config,
-            seed=(seed + n_agent) * (n_agent + 1) * 1000)
-        envs.append(env)
-        agents.append(agent)
-
-    # train along each session, and report average performance after each one
-    for n_epoch in range(n_epochs):
-        # learn one epoch per agent
-        returns = [
-            agent.learn_one_epoch(
-                n_episodes=train_eps,
-                perturbation_decay=perturbation_decay,
-                seed=seed + train_eps * n_epoch,
-                raises=True)
-            for agent in agents
-        ]
-        # import joblib as jl
-        # returns = jl.Parallel(n_jobs=-1)(
-        #     jl.delayed(QuadRotorLSTDQAgent.learn_one_epoch)(
-        #         agent,
-        #         n_episodes=train_eps,
-        #         perturbation_decay=perturbation_decay,
-        #         seed=seed + train_eps * n_epoch,
-        #         raises=True
-        #     ) for agent in agents
-        # )
-
-        # average peformance over last n episodes and over all agents
-        mean_return = np.mean(returns)
-
-        # report current average performance
-        trial.report(mean_return, n_epoch)
-        if trial.should_prune():
-            raise optuna.TrialPruned()
+            seed=seed + n_agent * n_agents,
+            after_epoch_callback=after_epoch_callback
+        ) for n_agent in range(n_agents)
+    )
 
     # return last session's averaged performance as result of the trial
-    return mean_return
+    if np.isnan(returns).any():
+        raise ValueError('Nan detected.')
+    return returns[:, -1].mean()
 
 
 if __name__ == '__main__':
