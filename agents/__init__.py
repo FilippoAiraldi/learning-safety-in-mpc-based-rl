e2808bcffda64111743dc3416582bb41998546de
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-09-19 10:11:06 +0200

    renamed some project files

--- agents/__init__.py
+++ agents/__init__.py
@@ -1,11 +1,11 @@
 from agents.replay_memory import ReplayMemory
 from agents.rl_parameters import RLParameter, RLParameterCollection
 
-from agents.quad_rotor_PI_agent import QuadRotorPIAgent
-from agents.quad_rotor_LSTD_Q_agent import (
+from agents.quad_rotor_pi_agent import QuadRotorPIAgent
+from agents.quad_rotor_lstd_q_agent import (
     QuadRotorLSTDQAgent, QuadRotorLSTDQAgentConfig
 )
-from agents.quad_rotor_LSTD_DPG_agent import (
+from agents.quad_rotor_lstd_dpg_agent import (
     QuadRotorLSTDDPGAgent, QuadRotorLSTDDPGAgentConfig
 )
 
