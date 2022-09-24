1c75679bd1b985897302794de17af3dad95f9a0b
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-09-24 17:38:53 +0200

    updated tune.py

--- tune.py
+++ tune.py
@@ -1,39 +1,45 @@
 import argparse
-# import contextlib
-# import joblib as jl
 import numpy as np
 import optuna
-# import os
-# import shutil
-from agents import QuadRotorLSTDQAgent, QuadRotorLSTDDPGAgent
+from agents import (
+    QuadRotorLSTDQAgent,
+    QuadRotorLSTDDPGAgent,
+    QuadRotorGPSafeLSTDQAgent
+)
+from agents.quad_rotor_base_learning_agent import (
+    QuadRotorBaseLearningAgent,
+    UpdateError
+)
 from envs import QuadRotorEnv
 from mpc import MPCSolverError
 from util import io
-# from typing import Any, Callable
+from typing import Type, TypeVar
+
+
+AgentType = TypeVar('AgentType', bound=QuadRotorBaseLearningAgent)
 
 
 def objective(
     trial: optuna.Trial,
+    agent_cls: Type[AgentType],
     n_epochs: int,
     n_agents: int,
     max_ep_steps: int,
     seed: int,
 ) -> float:
     # suggest parameters for this trial
-    gamma = 1.0 #trial.suggest_float(
-        # 'gamma', 0.98, 1.0, log=True)
-    lr = trial.suggest_float(
-        'lr', 1e-3, 5e-1, log=True)
-    train_eps = 5 # trial.suggest_categorical(
-        # 'train_eps', [1, 5, 10, 20])
+    gamma = trial.suggest_float('gamma', 0.98, 1.0, log=True)
+    lr = trial.suggest_float('lr', 1e-3, 5e-1, log=True)
+    train_eps = 5  # trial.suggest_categorical(
+    # 'train_eps', [1, 5, 10, 20])
     max_perc_update = trial.suggest_categorical(
         'max_perc_update', [0.2, 0.5, 1.0, np.inf])
-    replay_maxlen_factor = 10 # trial.suggest_int(
-        # 'replay_maxlen_factor', 1, n_epochs // 2)
+    replay_maxlen_factor = 10  # trial.suggest_int(
+    # 'replay_maxlen_factor', 1, n_epochs // 2)
     replay_mem_sample_size = trial.suggest_float(
         'mem_sample_size', 0.2, 0.8, step=0.1)
-    replay_include_last_factor = 0 # trial.suggest_int(
-        # 'replay_include_last_factor', 0, 1)
+    replay_include_last_factor = 1  # trial.suggest_int(
+    # 'replay_include_last_factor', 0, 1)
     perturbation_decay = trial.suggest_float(
         'perturbation_decay', 0.5, 1.0, step=0.1)
 
@@ -47,7 +53,7 @@ def objective(
         'replay_include_last': train_eps * replay_include_last_factor
     }
     envs: list[QuadRotorEnv] = []
-    agents: list[QuadRotorLSTDDPGAgent] = []
+    agents: list[agent_cls] = []
     for n_agent in range(n_agents):
         env = QuadRotorEnv.get_wrapped(
             max_episode_steps=max_ep_steps,
@@ -55,7 +61,7 @@ def objective(
             normalize_observation=False,
             normalize_reward=False)
 
-        agent = QuadRotorLSTDDPGAgent(
+        agent = agent_cls(
             env=env,
             agentname=f'LSTDQ_{n_agent}',
             agent_config=agent_config,
@@ -70,8 +76,7 @@ def objective(
             agent.learn_one_epoch(
                 n_episodes=train_eps,
                 perturbation_decay=perturbation_decay,
-                seed=seed + train_eps * n_epoch,
-                raises=True)
+                seed=seed + train_eps * n_epoch)
             for agent in agents
         ]
 
@@ -87,115 +92,6 @@ def objective(
     return mean_return
 
 
-# def train_one_agent(
-#     n_agent: int,
-#     n_epochs: int,
-#     n_episodes: int,
-#     max_ep_steps: int,
-#     perturbation_decay: float,
-#     agent_config: dict[str, Any],
-#     seed: int,
-#     after_epoch_callback: Callable[[int, int, float], None]
-# ) -> None:
-#     env = QuadRotorEnv.get_wrapped(
-#         max_episode_steps=max_ep_steps,
-#         record_data=False,
-#         normalize_observation=False,
-#         normalize_reward=False)
-#     agent = QuadRotorLSTDQAgent(
-#         env=env,
-#         agentname=f'LSTDQ_{n_agent}',
-#         agent_config=agent_config,
-#         seed=seed * (n_agent + 1) * 1000)
-#     for n_epoch in range(n_epochs):
-#         current_returns = agent.learn_one_epoch(
-#             n_episodes=n_episodes,
-#             perturbation_decay=perturbation_decay,
-#             seed=seed + n_episodes * n_epoch,
-#             raises=True)
-#         after_epoch_callback(n_agent, n_epoch, current_returns.mean())
-
-
-# def objective(
-#     trial: optuna.Trial,
-#     n_epochs: int,
-#     n_agents: int,
-#     max_ep_steps: int,
-#     seed: int,
-# ) -> float:
-#     # suggest parameters for this trial
-#     gamma = trial.suggest_float(
-#         'gamma', 0.98, 1.0, log=True)
-#     lr = trial.suggest_float(
-#         'lr', 1e-3, 5e-1, log=True)
-#     train_eps = trial.suggest_categorical(
-#         'train_eps', [1, 5, 10, 20])
-#     max_perc_update = trial.suggest_categorical(
-#         'max_perc_update', [0.2, 0.5, 1.0, np.inf])
-#     replay_maxlen_factor = trial.suggest_int(
-#         'replay_maxlen_factor', 1, n_epochs // 2)
-#     replay_mem_sample_size = trial.suggest_float(
-#         'mem_sample_size', 0.2, 0.8, step=0.1)
-#     replay_include_last_factor = trial.suggest_int(
-#         'replay_include_last_factor', 0, 1)
-#     perturbation_decay = trial.suggest_float(
-#         'perturbation_decay', 0.5, 0.99)
-
-#     # create envs and agents
-#     agent_config = {
-#         'gamma': gamma,
-#         'lr': lr,
-#         'max_perc_update': max_perc_update,
-#         'replay_maxlen': train_eps * replay_maxlen_factor,
-#         'replay_sample_size': replay_mem_sample_size,
-#         'replay_include_last': train_eps * replay_include_last_factor
-#     }
-
-#     # setup tracking of intermediate results (use memmap to share between
-#     # subprocesses)
-#     # https://joblib.readthedocs.io/en/latest/auto_examples/parallel_memmap.html
-#     folder = os.path.join('.', 'tuning', 'memmap', f'trial{trial._trial_id}')
-#     with contextlib.suppress(FileNotFoundError):
-#         shutil.rmtree(folder)
-#     with contextlib.suppress(FileExistsError):
-#         os.mkdir(folder)
-#     returns = np.memmap(
-#         os.path.join(folder, 'returns_memmap'),
-#         dtype=float,
-#         shape=(n_agents, n_epochs),
-#         mode='w+')
-#     returns.fill(np.nan)
-
-#     def after_epoch_callback(n_agent: int, n_epoch: int, mean_return: float):
-#         # store this agent's returns in this epoch
-#         returns[n_agent, n_epoch] = mean_return
-
-#         # if all agents have gone through this epoch, report its performance
-#         if not np.isnan(returns[:, n_epoch]).any():
-#             trial.report(returns[:, n_epoch].mean(), n_epoch)
-#             if trial.should_prune():
-#                 raise optuna.TrialPruned()
-
-#     # launch parallel training of each agent
-#     jl.Parallel(n_jobs=-1)(
-#         jl.delayed(train_one_agent)(
-#             n_agent=n_agent,
-#             n_epochs=n_epochs,
-#             n_episodes=train_eps,
-#             max_ep_steps=max_ep_steps,
-#             perturbation_decay=perturbation_decay,
-#             agent_config=agent_config,
-#             seed=seed + n_agent * n_agents,
-#             after_epoch_callback=after_epoch_callback
-#         ) for n_agent in range(n_agents)
-#     )
-
-#     # return last epoch's averaged performance as result of the trial
-#     if np.isnan(returns).any():
-#         raise ValueError('Nan detected.')
-#     return returns[:, -1].mean()
-
-
 if __name__ == '__main__':
     # parse fixed parameters of the tuning process
     parser = argparse.ArgumentParser()
@@ -205,8 +101,7 @@ if __name__ == '__main__':
                         help='Number of training epochs.')
     parser.add_argument('--max_ep_steps', type=int, default=50,
                         help='Maximum number of steps per episode.')
-    parser.add_argument('--seed', type=int, default=42, help='RNG seed.')
-    #
+    parser.add_argument('--seed', type=int, default=1909, help='RNG seed.')
     parser.add_argument('--trials', type=int, default=20,
                         help='Number of Optuna trials.')
     parser.add_argument('--timeout', type=int, default=6 * 60 * 60,
@@ -217,11 +112,12 @@ if __name__ == '__main__':
 
     # create study and begin optimization
     study = optuna.create_study(
-        study_name='lstdq-tuning',
+        study_name='gp-safe-lstdq-tuning',
         pruner=optuna.pruners.SuccessiveHalvingPruner(),
         direction='minimize')
     obj_fn = lambda trial: objective(
         trial,
+        agent_cls=QuadRotorGPSafeLSTDQAgent,
         n_epochs=args.epochs,
         n_agents=args.agents,
         max_ep_steps=args.max_ep_steps,
@@ -231,7 +127,7 @@ if __name__ == '__main__':
         n_trials=args.trials,
         timeout=args.timeout,
         n_jobs=args.n_jobs,
-        catch=(MPCSolverError,),
+        catch=(MPCSolverError, UpdateError),
         show_progress_bar=False)
 
     # save results
