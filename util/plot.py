c8ab956cc54a271d56cabe8bcd7f43d20f67e48a
Filippo Airaldi <f.airaldi@tudelft.nl>
2022-10-07 13:40:44 +0200

    small adjustment

--- util/plot.py
+++ util/plot.py
@@ -291,7 +291,7 @@ def performance(
         ax = fig.axes[0]
 
     rewards: np.ndarray = jaggedstack([env.cum_rewards for env in envs])
-    y = np.nanmean(rewards, axis=0) 
+    y = np.nanmean(rewards, axis=0)
     # y = np.nanmedian(rewards, axis=0)
     episodes = np.arange(rewards.shape[1]) + 1
 
